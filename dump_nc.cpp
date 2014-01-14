cb78a8cd34c25c2b5e7c67b066ef1f77662c51c8
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2014-01-14 18:46:32 +0100

    Changed GPL headers

--- dump_nc.cpp
+++ dump_nc.cpp
@@ -1,3 +1,25 @@
+/* ======================================================================
+   LAMMPS NetCDF dump style
+   https://github.com/pastewka/lammps-netcdf
+   Lars Pastewka, lars.pastewka@iwm.fraunhofer.de, and others
+   See the AUTHORS file in the top-level Atomistica directory.
+
+   Copyright (2011-2013) Fraunhofer IWM
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   ====================================================================== */
+
 /* ----------------------------------------------------------------------
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
    http://lammps.sandia.gov, Sandia National Laboratories
@@ -9,10 +31,6 @@
    the GNU General Public License.
 
    See the README file in the top-level LAMMPS directory.
-------------------------------------------------------------------------- */
-
-/* ----------------------------------------------------------------------
-   Contributing author: Lars Pastewka (Johns Hopkins University, Fh-IWM)
 ------------------------------------------------------------------------- */
 
 #include <netcdf.h>
